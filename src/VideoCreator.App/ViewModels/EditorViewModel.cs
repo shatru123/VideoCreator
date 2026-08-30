b32011af30513be0eea09730b0ceafdcbd04c11a
Shatru123 <ambhoreshatrughna@gmail.com>
2026-08-30 21:14:49 +0530

    fix: resolve ICommand ambiguity and missing collections namespace in EditorViewModel

--- src/VideoCreator.App/ViewModels/EditorViewModel.cs
+++ src/VideoCreator.App/ViewModels/EditorViewModel.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using ICommand = System.Windows.Input.ICommand;
 using Avalonia.Threading;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
